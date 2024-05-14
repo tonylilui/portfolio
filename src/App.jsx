686541cd4c5bf5de39f8c524725461a8c624d8bc
Tony Li <tony.lilui416@gmail.com>
2024-05-13 22:25:44 -0400

    fixed title logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>tony</h1>
-    </>
+    <div className="bg-slate-900">
+      <Navbar></Navbar>
+    </div>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+const Navbar = () => {
+  return (
+    <div className="">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Projects</li>
+        <li>Home</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
